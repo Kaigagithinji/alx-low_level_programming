7facb67e254e487da5bf3955553909448ad9c5fd
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 11:45:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * 
  * Description: 'printing using puts'
  * Parameters: String
- * Return: Always 0 (Success)
- */ int main(void)
+ * Return: Always 0 (Success)*/
+int main(void)
 {char my_str[] = "\"Programming is like building a multilingual puzzle";
 puts(my_str);
 return (0);
