06748cf256b45c148eafc28c491fc0bcdc85374e
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 12:58:48 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - entry point
  * description - use printf,do not use puts
  * parameter - string
+ *return: always 0 (success)
  */
-int main(void) /*return: always 0 (success)*/
+int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0); }
