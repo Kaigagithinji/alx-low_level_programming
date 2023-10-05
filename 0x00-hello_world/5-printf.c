a6fec1dd5a2f9070b6338b92dbd9e16fdfeaeec0
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 12:48:17 -0700

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main - entry point
+ * description - use printf,do not use puts
+ * parameter - string
+ */
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);}
