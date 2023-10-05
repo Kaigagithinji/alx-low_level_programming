9a8d8bfc9e0b4f8a5145867befaf5eaea86f580e
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 13:45:58 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>                                                                                                                  
-                                                                                                                                       
-/**                                                                                                                                 
-* main - Entry point                                                                                                               
-* Return: Always 0 (Success)                                                                                                       
-*/                                                                                                                                 
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */                                                                                                                                 
 int main(void)                                                                                                                      
 {printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
