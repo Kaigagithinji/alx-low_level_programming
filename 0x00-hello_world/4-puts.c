a19cb474ff085e2eb5fd44179072cfd7c1eee93a
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 11:31:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void)/*printing using puts*i/
+int main(void) /*printing using puts*i/
 {char my_str[] = "\"Programming is like building a multilingual puzzle";
 puts(my_str);
 return (0);
