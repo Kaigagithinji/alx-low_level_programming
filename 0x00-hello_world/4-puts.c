91f23bf057e2bf658a65951721ad52aa2d09dc8f
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-05 11:20:21 -0700

    1-compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(void)
-{ 
-char my_str[] = "\"Programming is like building a multilingual puzzle";
-
+{char my_str[] = "\"Programming is like building a multilingual puzzle";
 puts(my_str);
 return(0);
 }
