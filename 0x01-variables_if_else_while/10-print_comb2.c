e0afe90719b4be1ccf9dd48ca8c780428de171ee
mrima <mrimamss@gmail.com>
2023-10-11 13:10:02 +0300

    combination 2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: combination 2
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		int x = 48;
+
+		while (x < 58)
+		{
+			putchar(i);
+			putchar(x);
+			if (i == 57 && x == 57)
+			{
+				break;
+			}
+				putchar(44);
+				putchar(32);
+			x++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
