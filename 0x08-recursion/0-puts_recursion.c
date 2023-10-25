cfb743878c60a69e3daae82638f2777de24daeb5
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-25 23:24:02 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
+void _puts_recursion(char *s);
 /**
  * main - check the code
- *
- * Return: Always 0.
+ * return: Always 0.
  */
-void _putchar (char *s);
 int main (void)
 
 {
 	char *memory = s;
-	int _putchar(int "Puts with recursion");
+	int _puts_recursion("First, solve the problem. Then, write the code");
 	return(0);
 }	
 
