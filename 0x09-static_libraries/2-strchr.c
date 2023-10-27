2f6b2643328bdb20a6ff4ee14e587262eb823285
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-27 15:26:06 +0300

    static libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strchr - This is my function
+ *@s: This my string of char
+ *@c: This is my character
+ *Return: This is my resulti
+ */
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == c)
+			return (s);
+		if (*s == 0)
+			return (NULL);
+	}
+	return (NULL);
+}
