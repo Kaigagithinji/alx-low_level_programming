f88a76e1b7eb8c5c5f8720709991f7c3cbc96f2d
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-30 15:05:56 +0300

    malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	_2D = malloc(sizeof(int *) * height);
 	if (_2D == NULL)
-			return (NULL);
+		return (NULL);
 	for (h_index = 0; h_index < height; h_index++)
 	{
 	_2D[h_index] = malloc(sizeof(int) * width);
@@ -30,10 +30,10 @@ int **alloc_grid(int width, int height)
 	return (NULL);
 																						}
 																						}
-																						for (h_index = 0; h_index < height; h_index++)
-																						{
-																						for (w_index = 0; w_index < width; w_index++)
-																						_2D[h_index][w_index] = 0;
-																						}																					return (_2D);
-																					}
+	for (h_index = 0; h_index < height; h_index++)
+	{
+		for (w_index = 0; w_index < width; w_index++)
+			_2D[h_index][w_index] = 0;																}
+	return (_2D);
+}
 
