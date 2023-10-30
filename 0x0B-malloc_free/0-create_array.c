a1cf82c51fae75015ee940516548fda687ed8d9c
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-30 12:13:38 +0300

    malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,13 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
         unsigned int index;
+
         if (size == 0)
-	       return(NULL);
+	       return (NULL);
         array = malloc(sizeof(char) * size);
 	if (array == NULL)
-	       return(NULL);
+	       return (NULL);
         for(index = 0; index < size; index++)
 	       array[index] = c;
-        return(array);
+        return (array);
 }	
