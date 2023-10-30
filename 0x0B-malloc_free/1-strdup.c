cf81b00e3a4e2eada2b41cccc5db8930bb434cc1
Kaigagithinji <kaigagithinji@gmail.com>
2023-10-30 11:59:57 +0300

    malloc free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a new string 
+ *           which is a duplicate of the string
+ * @str - the string to be duplicated
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *         Otherwise - a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int index, size = 0;
+	
+	if (str == NULL)
+		return(NULL);
+	
+	for (index = 0; str[index]; index++)
+		size++;
+
+	copy = malloc(sizeof(char) * (size + 1));
+
+	if (copy == NULL)
+		return(NULL);
+	for (index = 0; str[index]; index++)
+		copy[index] = str[index];
+
+	copy[size] = '\0';
+
+	return(copy);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADERFILE
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 #endif
