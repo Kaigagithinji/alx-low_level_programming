dd4535eba94acf18663bea10dc44869c187d18e1
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-06 15:50:44 +0300

    macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,12 @@
 #ifndef macro
 #define macro
+
 /**
  * description: header file that defines a macro named SIZE
  *               as an abbreviation for the token 1024
+ *
  */
+
 #define SIZE 1024
+
 #endif
