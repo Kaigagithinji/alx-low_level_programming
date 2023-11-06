8a057bfcd553e8e8d734227ba28c193081be2b7f
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-06 16:52:05 +0300

    macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef sum_h
+#define sum_h
+
+/**
+ * description: function-like macro SUM(x, y) 
+ *              that computes the sum of the numbers x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
