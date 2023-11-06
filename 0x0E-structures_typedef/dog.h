20d1bfda1f35229169197725aeffc9da63021460
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-06 18:39:02 +0300

    macro

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * description -  Define a new type struct dog 
+ *                in the header file
+ * @struct dog - a new type defining a dog
+ * @name: name of the dog in char
+ * @age: age of the dog in float
+ * @owner: name of the owner in char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
