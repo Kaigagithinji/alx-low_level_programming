5187930ffea1abeab5df06df82555a8c05fc4e91
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-06 16:16:41 +0300

    macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * description - program that prints the name of the file 
+ *               it was compiled from, followed by a new line
+ *@s: the string to be printed
+ * @__FILE__: macro that return name of the current file
+ */              
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
