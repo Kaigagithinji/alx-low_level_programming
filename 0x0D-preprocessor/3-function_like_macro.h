c0d1323df0c7d11d1f2760c0bf0d9583fed0862e
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-06 16:37:30 +0300

    macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef function_like_macro
+#define function_like_macro
+
+/**
+ * description - a function-like macro ABS(x) 
+ *               that computes the absolute value 
+ *               of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
