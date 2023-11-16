73f6ba7f137ecd4ced4318c2d78a57cbd3eb6dc8
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-16 13:24:14 +0300

    number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+/**
+ * File: 1-list_len.c
+ * Author: Kaiga Githinji
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - function that prints the number of nodes
+ * @h: The list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *node_pointer = h; 
+	size_t count = 0;
+
+	while (node_pointer != NULL)
+	{
+		count++;
+		node_pointer = node_pointer->next;
+	}
+	return count;
+}
+
