cc99c564b6a0a9969944e16ccbbe95a7798199c4
Kaigagithinji <kaigagithinji@gmail.com>
2023-11-17 15:33:04 +0300

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,50 @@
-#include "lists.h"
-#include <stdio.h>
-
-/**
- * add_node_end - a function that adds a new node at the end of a list
- * @head: pointer to the head node of the list
- * @str: the string to be duplicated
+/*
+ * File: 3-add_node_end.c
+ * Auth: Kaiga Githinji
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+#include "lists.h"
 
-	list_t *new_node = malloc(sizeof(list_t));
+/**
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+list_t *add_node_end(list_t **head, const char *str)
 {
+	char *duplicate;
+	int len;
+	list_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		return NULL;
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	return (NULL);
+
+	duplicate = strdup(str);
+	if (str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
-	new_node->len = (unsigned int)strlen(str);
+	for (len = 0; str[len];)
+		len++;
+
+	new_node->str = duplicate;
+	new_node->len = len;
 	new_node->next = NULL;
+
 	if (*head == NULL)
-	{
 		*head = new_node;
-		return new_node;
-	}
-	list_t *node_pointer = *head;
-	while (node_pointer->next != NULL)
+	else
 	{
-		node_pointer = node_pointer->next;
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
 	}
-	node_pointer->next = new_node;
-
-	return new_node;
+	return (*head);
 }
-
